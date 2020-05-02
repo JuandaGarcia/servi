15d52a24844280ce8af187e36f430732469a0bfa
Juan David <gottygarcia@gmail.com>
2020-05-02 00:38:01 -0500

    panel administrador conectado a la API

--- src/Pages/AdminUsers.js
+++ src/Pages/AdminUsers.js
@@ -1,31 +1,126 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import NavBar from '../components/AdminNavBar'
 import Fichero from '../components/fichero'
+import axios from 'axios'
+import Swal from 'sweetalert2'
+
 const AdminUsers = () => {
-	return <div>
-		<NavBar/>
-		<div className="panel_central">
-			<div >
-				<h1>
-				Usuarios nuevos
-				</h1>
-				<input type="text"/>
+	const [data, setData] = useState([])
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState(false)
+
+	useEffect(() => {
+		fetchData()
+	}, [])
+
+	const fetchData = async () => {
+		setLoading(true)
+		await axios
+			.get('http://localhost:5000/prestador/todos')
+			.then((res) => {
+				setLoading(false)
+				setData(res.data)
+			})
+			.catch((err) => {
+				setLoading(false)
+				setError(true)
+			})
+		setLoading(false)
+	}
+
+	const deleteData = async (id) => {
+		setLoading(true)
+		await axios
+			.delete(`http://localhost:5000/prestador/${id}`)
+			.then((res) => {
+				setLoading(false)
+			})
+			.catch((err) => {
+				setLoading(false)
+				setError(true)
+			})
+		setLoading(false)
+	}
+
+	const putData = async (id) => {
+		setLoading(true)
+		await axios
+			.put(`http://localhost:5000/prestador/${id}`)
+			.then((res) => {
+				setLoading(false)
+			})
+			.catch((err) => {
+				setLoading(false)
+				setError(true)
+			})
+		setLoading(false)
+	}
+
+	const Modal = (user) => {
+		console.log(data[user]._id)
+
+		const swalWithBootstrapButtons = Swal.mixin({
+			customClass: {
+				confirmButton: 'btn btn-success',
+				cancelButton: 'btn btn-danger',
+			},
+		})
+
+		swalWithBootstrapButtons
+			.fire({
+				title: `Usuario: ${data[user].email}`,
+				text: `Nombre: ${data[user].nombre} | Correo: ${data[user].email} | Teléfono: ${data[user].telefono} | ID: ${data[user]._id}`,
+				icon: 'info',
+				showCancelButton: true,
+				confirmButtonText: 'Aceptar usuario',
+				cancelButtonText: 'Rechazar usuario',
+				reverseButtons: true,
+			})
+			.then((result) => {
+				if (result.value) {
+					putData(data[user]._id)
+					fetchData()
+					swalWithBootstrapButtons.fire(
+						'Aceptado',
+						'El usuario fue aceptado en la plataforma.',
+						'success'
+					)
+				} else if (
+					/* Read more about handling dismissals below */
+					result.dismiss === Swal.DismissReason.cancel
+				) {
+					deleteData(data[user]._id)
+					fetchData()
+					swalWithBootstrapButtons.fire(
+						'Rechazado',
+						'El usuario fue rechazado en la plataforma.',
+						'info'
+					)
+				}
+			})
+	}
+
+	return (
+		<div>
+			<NavBar />
+			<div className="panel_central">
+				<div>
+					<h1>Usuarios nuevos</h1>
+					<input type="text" />
+				</div>
+			</div>
+
+			<div className="rendijas">
+				{data.map((user, index) => {
+					return (
+						<div onClick={() => Modal(index)}>
+							<Fichero name={user.nombre} email={user.email} />
+						</div>
+					)
+				})}
 			</div>
 		</div>
-
-		<div className="rendijas">
-			<Fichero name={"kevin"} fecha={"20/04/2020"}/>
-			<Fichero/>
-			<Fichero/>
-			<Fichero/>
-			<Fichero/>
-			<Fichero/>
-			<Fichero/>
-			<Fichero/>
-			<Fichero/>
-		</div>
-		
-	</div>
+	)
 }
 
 export default AdminUsers
--- src/components/fichero.js
+++ src/components/fichero.js
@@ -1,21 +1,15 @@
 import React from 'react'
 
-
-const fichero = (props) =>{
-
-    return(
-        <div className="fichero">
-            <img src="http://localhost:3000/img/user.png" alt="" />
-            <div >
-                <h5>
-                    {props.name}
-                </h5>
-                <h6>
-                    Fecha registro: {props.fecha}
-                </h6>
-            </div>
-        </div> 
-    )
+const fichero = (props) => {
+	return (
+		<div className="fichero">
+			<img src="http://localhost:3000/img/user.png" alt="" />
+			<div>
+				<h5>{props.name}</h5>
+				<h6>Correo: {props.email}</h6>
+			</div>
+		</div>
+	)
 }
 
 export default fichero
